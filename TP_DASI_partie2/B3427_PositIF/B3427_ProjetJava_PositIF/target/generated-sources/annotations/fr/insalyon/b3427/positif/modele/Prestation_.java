bd8b97e23905d8d4be81464125592789971b335d
chris23insa <chris23insa@users.noreply.github.com>
2018-05-18 15:27:03 +0200

    chgt

--- TP_DASI_partie2/B3427_PositIF/B3427_ProjetJava_PositIF/target/generated-sources/annotations/fr/insalyon/b3427/positif/modele/Prestation_.java
+++ TP_DASI_partie2/B3427_PositIF/B3427_ProjetJava_PositIF/target/generated-sources/annotations/fr/insalyon/b3427/positif/modele/Prestation_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-12T11:05:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-18T15:18:56")
 @StaticMetamodel(Prestation.class)
 public class Prestation_ { 
 
